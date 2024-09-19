4e4e9b0d89719f45e1ee48329660eeda71a84a5b
unknown <wl00795897@gmail.com>
2024-09-20 03:29:09 +0800

    modify random generator

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
-from flask_socketio import join_room, leave_room, send, SocketIO
+from flask_socketio import join_room, leave_room, send, emit, SocketIO
 from flask_cors import CORS
 import random
-from string import ascii_uppercase
+import string
 import io
 from base64 import encodebytes
 from PIL import Image, ImageFont, ImageDraw
@@ -29,17 +29,14 @@ loras = [
     {"name": "rabbit", "checkpoint": "pytorch_lora_weights.safetensors"},
     {"name": "pig", "checkpoint": "pytorch_lora_weights.safetensors"}
 ]
-candidate_labels = ['Happy', 'Sad', 'Angry', 'Unpleasant', 'Tired', 'Greeting', 'Sleep']
+candidate_labels = ['Happy', 'Sad', 'Angry', 'Tired', 'Hello', 'Congratulation', 'Thanks']
 
-def generate_unique_code(length):
-    while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
-        if code not in rooms:
-            break
-    
-    return code
+def code_gen(length):
+    code = ''.join(random.choices(string.digits, k=length))
+    if code not in rooms:
+        return code
+    else:
+        return code_gen(length)
 
 def get_response_image(image_path):
     pil_img = Image.open(image_path, mode='r') # reads the PIL image
@@ -65,7 +62,7 @@ def home():
         
         room = code
         if create != False:
-            room = generate_unique_code(4)
+            room = code_gen(4)
             rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
             return render_template("home.html", error="Room does not exist.", code=code, name=name)
@@ -92,13 +89,16 @@ def getStickers():
         pfc = data.get("input")
         if len(pfc.split()) == 1:
             prompt_word = pfc
+            print(f"Received input from client: {prompt_word}")
         else:
             result = classifier(pfc, candidate_labels)
             prompt_word = result['labels'][0]
+            print(f"Received input from client: {pfc}")
+            print(f"After ZSL: {prompt_word}")
         # prompt_animal = "otter"
         # result = classifier(data.get("input"), candidate_labels)
         # prompt_word = result['labels'][0]
-        print(f"Received input from client: {prompt_word}")
+
         # vae = AutoencoderKL.from_pretrained(
         # "madebyollin/sdxl-vae-fp16-fix"
         # ).to(torch.float16)
@@ -121,7 +121,8 @@ def getStickers():
                     adapter_name=selected_lora["name"]
                 )
                 lora.append(selected_lora["name"])
-            prompt_animal = lora[0]
+            # print(((i+1) * random.randint(1, 10000)) % 2)
+            prompt_animal = lora[(random.randint(1, 10000)) % 2]
             print(f"merged lora: {lora}, prompt_animal: {prompt_animal}")
             # activate both LoRAs and set adapter weights
             pipeline.set_adapters(lora, adapter_weights=[0.5, 0.5])
@@ -135,9 +136,9 @@ def getStickers():
             # pipeline.unet = torch.compile(pipeline.unet, mode="reduce-overhead", fullgraph=True)
             default_prompt = "dm-sticker"
             prompt = default_prompt + ", " + prompt_animal + ", " + prompt_word
-            negative_prompt = "alphabet, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name,(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, bad art, worst quality, worst details, poorly drawn face, fused face, cloned face, ugly eyes, ((imperfect eyes)), deformed pupils, deformed iris, extra eyes, oversized eyes, extra crus, fused crus, extra thigh, fused thigh, missing fingers, extra fingers, elongated fingers, amputation, disconnected limbs, artist signature, upside down, asymmetrical eyes"
+            negative_prompt = "word, alphabet, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name,(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, bad art, worst quality, worst details, poorly drawn face, fused face, cloned face, ugly eyes, ((imperfect eyes)), deformed pupils, deformed iris, extra eyes, oversized eyes, extra crus, fused crus, extra thigh, fused thigh, missing fingers, extra fingers, elongated fingers, amputation, disconnected limbs, artist signature, upside down, asymmetrical eyes"
 
-            image = pipeline(prompt = prompt,negative_prompt=negative_prompt, num_inference_steps=4, guidance_scale=1.5).images[0]
+            image = pipeline(prompt = prompt,negative_prompt=negative_prompt, num_inference_steps=8, guidance_scale=1.5).images[0]
             img_w, img_h = image.size
 
 
@@ -183,7 +184,7 @@ def image(data):
         "image_data": data,
         "type": "image"
     }
-    socketio.emit('image', content, room=room)
+    emit('image', content, room=room)
 
 @socketio.on("connect")
 def connect(auth):
